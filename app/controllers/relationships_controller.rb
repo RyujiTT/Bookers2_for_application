d9416f5e31b8daba97f43e90310708c240ce611e
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-05-15 05:57:23 +0000

    [add]フォロー機能

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,19 @@
 class RelationshipsController < ApplicationController
   def create
-  end 
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
   def destroy
-  end 
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.following_user
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.follower_user
+  end
 end
