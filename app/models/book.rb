1977f8d37befd2b4ccc143cd60227909d7934071
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-05-19 06:42:30 +0000

    [update]いいね数の多い順にする機能簡略化

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
   has_one_attached :profile_image
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  has_many :favorited_users, through: :favorites, source: :user
+  has_many :week_favorites, -> { where(created_at: ((Time.current.at_end_of_day - 6.day).at_beginning_of_day)..(Time.current.at_end_of_day)) }, class_name: 'Favorite'
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
