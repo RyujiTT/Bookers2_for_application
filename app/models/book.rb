59f0a45a86acc6cddc050e3d950d5f062bb21285
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-05-19 06:07:23 +0000

    [add]いいねの合計が多い順に表示

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ApplicationRecord
   has_one_attached :profile_image
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
 
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
